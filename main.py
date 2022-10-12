9cc82e4417897fa88e50ea695b5bdd322067ec90
Teerat-CH <81249482+Teerat-CH@users.noreply.github.com>
2022-10-12 10:34:27 +0700

    Test 3 return fizz

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-def fizzbuzz(num : int) -> str:
-    return "5"
+def fizzbuzz(number: int) -> str:
+    if number == 1:
+        return "1"
+    if number == 3:
+        return "fizz"
--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,8 +1,13 @@
-import unittest
 from main import fizzbuzz
-class TestFizzBuzz(unittest.TestCase):
 
-    def test_1_should_return_1(self):
-        result = fizzbuzz(1)
-        expected = "1"
-        self.assertEqual(result, expected)
+
+def test_1_should_return_1():
+    result = fizzbuzz(1)
+    expected = "1"
+    assert result == expected
+    
+
+def test_3_should_return_fizz():
+    result = fizzbuzz(3)
+    expected = "fizz"
+    assert result == expected
